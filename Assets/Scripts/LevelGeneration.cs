06077ce3dd98a0adb7febd001f6c87a5d98e0e74
warmoverdrive <sweetsthebear@gmail.com>
2020-10-11 18:47:53 -0700

    # Proc-Gen Alpha
    * Basic functionality of level gen
    * Known bug - backtracking over generated rooms

--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -8,17 +8,22 @@ public class LevelGeneration : MonoBehaviour
     [SerializeField]
     Transform parentGrid;
     [SerializeField]
-    int roomWidth = 32, roomHeight = 18, 
+    int roomWidth = 32, 
+        roomHeight = 18, 
         minX = 0, 
         maxX = 256, 
-        minY = 72, 
-        maxY;
+        minY = 0, 
+        maxY = 72;
     [SerializeField]
-    GameObject[] rooms;
+    LayerMask roomLayer;
+    [SerializeField]
+    GameObject[] rooms; // [0] -> LR, [1] -> LRD, [2] -> LRU, [3] -> LRUD
 
     private int direction;
     float timeBetweenRoom;
     public float startTimeBetweenRoom = 0.25f;
+
+    private bool stopGeneration = false;
 
     void Start()
     {
@@ -27,11 +32,12 @@ public class LevelGeneration : MonoBehaviour
         Instantiate(rooms[0], transform.position, Quaternion.identity, parentGrid);
 
         direction = Random.Range(0, 6);
+        Debug.Log(direction);
     }
 
     private void Update()
     {
-        if (timeBetweenRoom <= 0)
+        if (timeBetweenRoom <= 0 && !stopGeneration)
         {
             Move();
             timeBetweenRoom = startTimeBetweenRoom;
@@ -46,26 +52,106 @@ public class LevelGeneration : MonoBehaviour
     {
         if (direction == 0 || direction == 1)           // MOVE RIGHT
         {
-            Vector2 newPos = new Vector2(transform.position.x + roomWidth, transform.position.y);
-            transform.position = newPos;
+            if (transform.position.x < maxX)
+            {
+                Vector2 newPos = new Vector2(transform.position.x + roomWidth, transform.position.y);
+                transform.position = newPos;
+
+                int rand = Random.Range(0, rooms.Length);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity, parentGrid);
+
+                direction = Random.Range(0, 6); // Set direction to never be left
+                if (direction == 2) direction = 1;
+                else if (direction == 3) direction = 5;
+            }
+            else direction = 5;
         }
         else if (direction == 2 || direction == 3)      // MOVE LEFT
         {
-            Vector2 newPos = new Vector2(transform.position.x - roomWidth, transform.position.y);
-            transform.position = newPos;
+            if (transform.position.x > minX)
+            {
+                Vector2 newPos = new Vector2(transform.position.x - roomWidth, transform.position.y);
+                transform.position = newPos;
+
+                int rand = Random.Range(0, rooms.Length);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity, parentGrid);
+
+                direction = Random.Range(2, 6); // set direction to never be right
+            }
+            else direction = 5;
         }
-        else if (direction == 4)                        // MOVE UP
+        else if (direction == 4)                        // MOVE DOWN
         {
-            Vector2 newPos = new Vector2(transform.position.x, transform.position.y + roomHeight);
-            transform.position = newPos;
+            if (transform.position.y > minY)
+            {
+                //Check if current room has a bottom opening
+                // if not, destroy room and replace with one that does
+                Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, roomLayer);
+                Debug.Log(roomDetection);
+                if (roomDetection)
+                {
+                    if (roomDetection.GetComponent<RoomType>().roomType == RoomType.Type.LR ||
+                        roomDetection.GetComponent<RoomType>().roomType == RoomType.Type.LRU &&
+                        roomDetection != null)
+                    {
+                        roomDetection.GetComponent<RoomType>().DestroyRoom();
+
+                        int randBottomRoom = Random.Range(1, 4);
+                        if (randBottomRoom == 2) randBottomRoom = 3;
+                        Instantiate(rooms[randBottomRoom], transform.position, Quaternion.identity, parentGrid);
+                    }
+                }
+
+                Vector2 newPos = new Vector2(transform.position.x, transform.position.y - roomHeight);
+                transform.position = newPos;
+
+                int rand = Random.Range(2, rooms.Length);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity, parentGrid);
+
+                direction = Random.Range(0, 4); // set to only go left or right
+            }
+            else direction = 5;
         }
-        else if (direction == 5)                        // MOVE DOWN
+        else if (direction == 5)                        // MOVE UP
         {
-            Vector2 newPos = new Vector2(transform.position.x, transform.position.y - roomHeight);
-            transform.position = newPos;
+            if(transform.position.y < maxY)
+            {
+                //Check if current room has a top opening
+                // if not, destroy room and replace with one that does
+                Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, roomLayer);
+                Debug.Log(roomDetection);
+                if(roomDetection)
+                {
+                    if (roomDetection.GetComponent<RoomType>().roomType == RoomType.Type.LR ||
+                        roomDetection.GetComponent<RoomType>().roomType == RoomType.Type.LRD && 
+                        roomDetection != null)
+                    {
+                        Debug.Log("Destroying Room");
+                        roomDetection.GetComponent<RoomType>().DestroyRoom();
+
+                        int randTopRoom = Random.Range(2, 4);
+                        Debug.Log("Random Top Room is " + randTopRoom);
+                        Instantiate(rooms[randTopRoom], transform.position, Quaternion.identity, parentGrid);
+                    }
+                }
+
+                Vector2 newPos = new Vector2(transform.position.x, transform.position.y + roomHeight);
+                transform.position = newPos;
+
+                int rand = Random.Range(1, rooms.Length);
+                if (rand == 3) rand = 1;
+                Instantiate(rooms[rand], transform.position, Quaternion.identity, parentGrid);
+
+                direction = Random.Range(0, 6);
+                if (direction == 4) direction = 5;
+            }
+            else
+            {
+                //stop generation, make exit
+                stopGeneration = true;
+            }
         }
 
-        Instantiate(rooms[0], transform.position, Quaternion.identity, parentGrid);
-        direction = Random.Range(0, 6);
+        Debug.Log(direction);
     }
 }
---
+++ Assets/Scripts/RoomType.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomType : MonoBehaviour
+{
+    [SerializeField]
+    public Type roomType;
+
+    public enum Type
+    {
+        LR,
+        LRD,
+        LRU,
+        LRUD,
+        UD
+    }
+
+    public void DestroyRoom() { Destroy(gameObject); }
+}
