8c602e2a03f0548ab17fc59fb9f5ac0cf5c0467c
warmoverdrive <sweetsthebear@gmail.com>
2020-11-08 15:05:01 -0800

    # Menu and Seed Update 1
    * Set up Main Menu
    * Set up Seed Input
    * Hooked up button logic to parse input and Init the RNG with the custom seed
    * Set up logic to ignore invalid seed inputs and default back to internal seed

---
+++ Assets/Scripts/Game-Management/MenuLogic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuLogic : MonoBehaviour
+{
+    [SerializeField]
+    InputField seedField; 
+
+    public void StartGame()
+	{
+		ParseAndSetSeed();
+		SceneController.LoadGame();
+	}
+
+	public void QuitGame() => Application.Quit();
+
+	private void ParseAndSetSeed()
+	{
+		string seedStr = seedField.text;
+
+		if (int.TryParse(seedStr, out int seed))
+		{
+			Random.InitState(seed);
+		}
+	}
+}
