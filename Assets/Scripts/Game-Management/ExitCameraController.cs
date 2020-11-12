44b2a982b59c624128599949f4f339c377b2c4be
warmoverdrive <sweetsthebear@gmail.com>
2020-11-12 10:22:14 -0800

    # Exit Update 2
    * Set up camera switching between castle and exit
    
    TODO
    - Set up trigger for win screen
    - Set up win screen menu

---
+++ Assets/Scripts/Game-Management/ExitCameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ExitCameraController : MonoBehaviour
+{
+    CinemachineVirtualCamera exitCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        exitCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.GetComponent<PlayerStatusController>())
+		{
+			exitCamera.MoveToTopOfPrioritySubqueue();
+		}
+	}
+}
